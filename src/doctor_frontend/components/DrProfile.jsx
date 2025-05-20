bcf4a355d6e7f76840c048bcad9f54c0e2b739bc
Lord-Eternal <et3rnxl24@gmail.com>
2025-05-21 06:18:54 +0700

    minor push

---
+++ src/doctor_frontend/components/DrProfile.jsx
@@ -0,0 +1,111 @@
+import "../styles/Body.css";
+import dr1 from "../assets/dr-pho-hoang-dang-mich.PNG";
+
+const DrProfile = () => {
+  return (
+    <div className="background">
+      <div className="container">
+        <div className="doctor">
+          <div className="col1">
+            <div className="img">
+              <img src={dr1} alt="Ảnh 1" />
+            </div>
+            <div className="role">
+              <h3>Giáo sư, Phó giáo sư, Tiến sĩ, Cố vấn chuyên môn</h3>
+              <h1>Cố vấn chuyên môn Phó Hoàng Đăng Mịch</h1>
+              <button>Đăng ký khám</button>
+            </div>
+          </div>
+          <div className="general">
+            <h2>Giới thiệu</h2>
+            <p>
+              PGS.TS. Cố vấn chuyên môn Hoàng Đăng Mịch đã có hơn 32 năm kinh
+              nghiệm trong lĩnh vực Nội tiết.
+            </p>
+            <p>
+              Năm 2010, ông vinh dự được phòng hàm Phó giáo sư và là Ủy viên ban
+              chấp hành Hội Nội tiết và Đái tháo đường Việt Nam, đồng thời, ông
+              cũng là Chủ tịch Hội Nội tiết và Đái tháo đường Hải Phòng.
+            </p>
+            <p>
+              Hiện tại, PGS.TS. Cố vấn chuyên môn Hoàng Đăng Mịch đang giữ chức
+              vụ là cố vấn chuyên môn khối Nội tại khoa Khám bệnh và Nội khoa -
+              Bệnh viện Đa khoa Quốc tế Vinmec Hạ Long.
+            </p>
+          </div>
+          <h3 className="tong_quan">Tổng quan</h3>
+          <div className="line"></div>
+          <p>Tác phong diện mạo</p>
+          <div className="line"></div>
+          <p>Thái độ giao tiếp</p>
+          <div className="line"></div>
+          <p>Chuyên môn</p>
+          <div className="line"></div>
+          <h3 className="tong_quan">Chi tiết lượt đánh giá</h3>
+          <p>5*</p>
+          <p>4*</p>
+          <p>3*</p>
+          <p>2*</p>
+          <p>1*</p>
+
+          <h3 className="tong_quan">Nhận xét về bác sĩ</h3>
+        </div>
+
+        <div className="info">
+          <h2>Chuyên môn</h2>
+          <p>Cố vấn chuyên môn khối Nội</p>
+          <div className="line"></div>
+
+          <h2>Nơi làm việc</h2>
+          <p>Khoa khám bệnh & Nội khoa - Bệnh viện Đa khoa Vinmec Hạ Long</p>
+          <div className="line"></div>
+
+          <h2>Dịch vụ</h2>
+          <p>• Nội tiết - Đái Tháo Đường - Rối loạn chuyển hóa</p>
+          <p>
+            • Hội chứng thận hư, viêm cầu thận cấp tính và mạn tính, các bệnh
+            thận khác; bệnh viêm gan virus B và viêm gan virus C, bệnh gan do
+            rượu; bệnh viêm loét dạ dày tá tràng, bệnh Lupus ban đỏ hệ thống và
+            các bệnh tự miễn khác
+          </p>
+          <div className="line"></div>
+
+          <h2>Quá trình đào tạo</h2>
+          <p>1977 - 1983: Bác sĩ đa khoa - Học viện Quân y</p>
+          <p>1991 - 1993: Bác sĩ chuyên khoa I - Trường Đại học Y Hà Nội</p>
+          <p>1997 - 2003: Tiến sĩ Học viện Quân y</p>
+          <p>2010: Học hàm Phó giáo sư</p>
+          <div className="line"></div>
+
+          <h2>Kinh nghiệm làm việc</h2>
+          <p>Giám đốc - Bệnh viện Việt Tiệp Hải Phòng</p>
+          <p>Phó giám đốc chuyên môn - Bệnh viện Việt Tiệp - Hải Phòng</p>
+          <p>
+            Giám đốc Trung tâm Can thiệp tim và mạch máu - Bệnh viện Việt Tiệp -
+            Hải Phòng
+          </p>
+          <p>Trưởng khoa Quốc tế - Bệnh viện Việt Tiệp - Hải Phòng</p>
+          <p>
+            Trưởng khoa Thận - Nội tiết - Đái tháo đường - Bệnh viện Việt Tiệp -
+            Hải Phòng
+          </p>
+          <p>
+            Hiện nay: Cố vấn Chuyên môn khối Nội, khoa Khám bệnh & Nội khoa,
+            Bệnh viện Đa khoa Quốc tế Vinmec Hạ Long
+          </p>
+          <div className="line"></div>
+
+          <h2>Thành viên của các tổ chức</h2>
+          <p>Ủy viên ban chấp hành Hội Nội tiết và Đái tháo đường Việt Nam</p>
+          <p>Chủ tịch Hội Nội tiết và Đái tháo đường Hải Phòng</p>
+          <div className="line"></div>
+
+          <h2>Sách, báo và công trình nghiên cứu</h2>
+          <p>Trên 40 công trình nghiên cứu và báo cáo chuyên ngành</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DrProfile;
---
+++ src/doctor_frontend/components/DrSchedule.jsx
@@ -0,0 +1,138 @@
+import "../styles/Body.css";
+import React, { useState, useEffect } from "react";
+
+const DrSchedule = () => {
+  const [month, setMonth] = useState(0);
+  const [year, setYear] = useState(new Date().getFullYear());
+
+  const handleNext = () => {
+    if (month === 11) {
+      setMonth(0);
+      setYear(year + 1);
+    } else {
+      setMonth(month + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (month === 0) {
+      setMonth(11);
+      setYear(year - 1);
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
+  const months = [
+    "Tháng 1",
+    "Tháng 2",
+    "Tháng 3",
+    "Tháng 4",
+    "Tháng 5",
+    "Tháng 6",
+    "Tháng 7",
+    "Tháng 8",
+    "Tháng 9",
+    "Tháng 10",
+    "Tháng 11",
+    "Tháng 12",
+  ];
+
+  const [scheduleData, setScheduleData] = useState([]);
+
+  useEffect(() => {
+    // Thay đổi sau: sử dụng MongoDB để tạo db gồm day & task, sau đó cần kết hợp bổ sung chọn tháng & năm
+    const fetchData = async () => {
+      const response = [
+        {
+          day: "20",
+          task: "Họp nhóm, gửi báo cáo, làm việc với khách hàng",
+        },
+        {
+          day: "21",
+          task: "Code tính năng mới, kiểm tra hệ thống, viết tài liệu",
+        },
+        {
+          day: "22",
+          task: "Thảo luận dự án, tổng kết tuần, lập kế hoạch mới",
+        },
+      ];
+      setScheduleData(response);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="background">
+      <div className="container">
+        <div style={{ textAlign: "center", padding: "20px" }}>
+          <button onClick={handlePrev}>&lt;</button>
+          <span className="date">
+            {months[month]}, {year}
+          </span>
+          <button onClick={handleNext}>&gt;</button>
+        </div>
+        <h2>Ngày hẹn khám</h2>
+        <div className="scheduled">
+          {scheduleData.map((item, index) => (
+            <div key={index}>
+              {index !== 0 && <div className="line"></div>}
+              <div
+                style={{
+                  display: "flex",
+                  width: "100%",
+                  gap: "10px",
+                }}
+              >
+                <div
+                  className="day"
+                  style={{
+                    flex: 1,
+                    fontWeight: "bold",
+                    fontSize: "26px",
+                    textAlign: "left",
+                  }}
+                >
+                  {item.day}
+                </div>
+                <div style={{ flex: 4, textAlign: "left" }}>{item.task}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <h2 style={{ marginTop: "50px" }}>Lịch làm việc</h2>
+        <div className="worktime">
+          {scheduleData.map((item, index) => (
+            <div key={index}>
+              {index !== 0 && <div className="line"></div>}
+              <div
+                style={{
+                  display: "flex",
+                  width: "100%",
+                  gap: "10px",
+                }}
+              >
+                <div
+                  className="day"
+                  style={{
+                    flex: 1,
+                    fontWeight: "bold",
+                    fontSize: "26px",
+                    textAlign: "left",
+                  }}
+                >
+                  {item.day}
+                </div>
+                <div style={{ flex: 4, textAlign: "left" }}>{item.task}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DrSchedule;
