5ef9e0d3ce82426253932f5ecd8dce6748994633
Anhnguyen <baanh9343@gmail.com>
2025-05-28 16:18:07 +0700

    anhnguyen

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,11 +4,15 @@ import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/connectDB.js";
 import Patients from "./routes/patientRoutes.js";
 import dotenv from "dotenv";
+const PORT = process.env.PORT;
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/patients", Patients);
-app.use("/api/appointment/create", userRoutes);
+app.use("/api/appointment", userRoutes);
 
 connectDB();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
